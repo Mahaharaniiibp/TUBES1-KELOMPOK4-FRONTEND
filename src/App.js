1a3ff50e7acf0da7e70a95008f2b4246034435fb
Maharaniiibp <maharanibanicaputri@gmail.com>
2022-12-13 19:37:52 +0700

    Co-authored-by: Maskharor Amnan Nidhom <maskharor@users.noreply.github.com> Co-authored-by: Aufff <aufaazka@users.noreply.github.com> Co-authored-by: Masyita Andini <masyitaandinii@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import Index from './pages/index';
 import logo from './logo.svg';
 import './App.css';
+import Detail_aufa from './pages/detail_aufa';
+import Detail_andini from './pages/detail_andini';
+import Detail_maska from './pages/detail_maska';
+import Detail_rani from './pages/detail_rani';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Calculator from './pages/Calculator';
+// import Calculator from './pages/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className> 
+   <Router>
+    <Routes>
+      <Route path='/' element={<Index/>} />
+      <Route path='/detail_aufa' element={<Detail_aufa/>} />
+      <Route path='/detail_andini' element={<Detail_andini/>} />
+      <Route path='/detail_maska' element={<Detail_maska/>} />
+      <Route path='/detail_rani' element={<Detail_rani/>} />
+      <Route path='/Calculator' element={<Calculator/>} />
+     
+      
+    </Routes>
+   </Router>
+   </div> 
+  
+  )
+  ;
 }
 
 export default App;

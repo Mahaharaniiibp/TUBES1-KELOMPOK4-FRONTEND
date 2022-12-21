5c0b206471bcbd8124c80d4ac5f5288e83118ff8
mhrnibp <100181754+Mahaharaniiibp@users.noreply.github.com>
2022-12-22 00:30:17 +0700

    Update Calculator.jsx

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,104 +1,123 @@
-import React, { useRef, useEffect, useState } from 'react';
-
+import React, { useRef, useEffect, useState } from "react";
+import { btns, BTN_ACTIONS } from './btnConfig';
 import '../assets/css/calculator.css';
 
-import { btns, BTN_ACTIONS } from './btnConfig';
-
-const Calculator = () => {
-
+function App() {
     const btnsRef = useRef(null);
     const expRef = useRef(null);
-
-    const [expression, setExpression] = useState('');
-
+  
+    const [expression, setExpression] = useState("");
+    const operation = ["+", "-", "/", "*"];
+  
     useEffect(() => {
-        const btns = Array.from(btnsRef.current.querySelectorAll('button'));
-        btns.forEach(e => e.style.height = e.offsetWidth + 'px');
+      const btns = Array.from(btnsRef.current.querySelectorAll("button"));
+      btns.forEach((e) => (e.style.height = e.offsetWidth + "px"));
     }, []);
-
+  
     const btnClick = (item) => {
-        const expDiv = expRef.current;
-
-        if (item.action === BTN_ACTIONS.THEME) document.body.classList.toggle('dark');
-
-        if (item.action === BTN_ACTIONS.ADD) {
-            addAnimSpan(item.display);
-
-            const oper = item.display !== 'x' ? item.display : '*';
-            setExpression(expression + oper);
+      let expDiv = expRef.current;
+  
+      if (item.action === BTN_ACTIONS.THEME)
+        document.body.classList.toggle("dark");
+  
+      if (item.action === BTN_ACTIONS.ADD) {
+        const op = item.display !== "x" ? item.display : "*";
+        const value = expression.split("");
+  
+        addAnimSpan(expression + op);
+  
+        if (operation.includes(value[value.length - 1]) && op === "%") {
+          addAnimSpan(expression);
+          return;
         }
-
-        if (item.action === BTN_ACTIONS.DELETE) {
-            expDiv.parentNode.querySelector('div:last-child').innerHTML = '';
-            expDiv.innerHTML = '';
-
-            setExpression('');
+  
+        if (
+          operation.includes(value[value.length - 1]) &&
+          operation.includes(op)
+        ) {
+          value.splice(value.length - 1, 1, op);
+          let result = value.join("");
+  
+          setExpression(expression);
+          addAnimSpan(result);
+          return;
         }
-
-        if (item.action === BTN_ACTIONS.PERCENT) {
-            let output = Number(expression) / 100;
-            console.log(output);
-            setExpression(output)
-            expDiv.innerHTML = output;
+      }
+  
+      if (item.action === BTN_ACTIONS.DELETE) {
+        expDiv.parentNode.querySelector("div:last-child").innerHTML = "";
+        expDiv.innerHTML = "";
+  
+        setExpression("");
+      }
+  
+      if (item.action === BTN_ACTIONS.DEL) {
+        let output = expression.split("");
+        output.pop();
+        let result = output.join("");
+  
+        expDiv.innerHTML = result;
+        setExpression(result);
+      }
+  
+      if (item.action === BTN_ACTIONS.CALC) {
+        if (expression.trim().length <= 0) return;
+  
+        expDiv.parentNode.querySelector("div:last-child").remove();
+  
+        const cloneNode = expDiv.cloneNode(true);
+        expDiv.parentNode.appendChild(cloneNode);
+  
+        const transform = `translateY(${
+          -(expDiv.offsetHeight + 10) + "px"
+        }) scale(0.4)`;
+  
+        try {
+          let value = "";
+          if (expression.includes("%")) {
+            value = expression.replace("%", "/100");
+          } else {
+            value = expression;
+          }
+  
+          let res = eval(value);
+  
+          setExpression(res.toString());
+          setTimeout(() => {
+            cloneNode.style.transform = transform;
+            expDiv.innerHTML = "";
+            addAnimSpan(Math.floor(res * 100000000) / 100000000);
+          }, 200);
+        } catch {
+          setTimeout(() => {
+            cloneNode.style.transform = transform;
+            cloneNode.innerHTML = "Syntax err";
+          }, 200);
+        } finally {
+          console.log("calc complete");
         }
-
-        if (item.action === BTN_ACTIONS.DEL) {
-            let output = expression.split('');
-            output.pop()
-            let result = output.join('');
-            console.log(result)
-
-            expDiv.innerHTML = result;
-            setExpression(result)
-        }
-
-        if (item.action === BTN_ACTIONS.CALC) {
-            if (expression.trim().length <= 0) return;
-
-            expDiv.parentNode.querySelector('div:last-child').remove();
-
-            const cloneNode = expDiv.cloneNode(true);
-            expDiv.parentNode.appendChild(cloneNode);
-
-            const transform = `translateY(${-(expDiv.offsetHeight + 10) + 'px'}) scale(0.4)`;
-
-            try { 
-
-                let res = eval(expression);
-
-                setExpression(res.toString());
-                setTimeout(() => {
-                    cloneNode.style.transform = transform;
-                    expDiv.innerHTML = '';
-                    addAnimSpan(Math.floor(res * 100000000) / 100000000);
-                }, 200);
-            } catch {
-                setTimeout(() => {
-                    cloneNode.style.transform = transform;
-                    cloneNode.innerHTML = 'Syntax err';
-                }, 200);
-            } finally {
-                console.log('calc complete');
-            }
-        }
-    }
-
+      }
+    };
+  
     const addAnimSpan = (content) => {
-        const expDiv = expRef.current;
-        const span = document.createElement('span');
-
-        span.innerHTML = content;
-        span.style.opacity = '0';
-        expDiv.appendChild(span);
-
-        const width = span.offsetWidth + 'px';
-        span.style.width = '0';
-
-        setTimeout(() => {
-            span.style.opacity = '1';
-            span.style.width = width;
-        }, 100);
-    }
+      const expDiv = expRef.current;
+      const span = document.createElement("span");
+  
+      expDiv.innerHTML = "";
+      span.innerHTML = content;
+      span.style.opacity = "0";
+      expDiv.appendChild(span);
+  
+      setExpression(content.toString());
+  
+      const width = span.offsetWidth + "px";
+      span.style.width = "0";
+  
+      setTimeout(() => {
+        span.style.opacity = "1";
+        span.style.width = width;
+      }, 100);
+    };
 
     return (
         <>
@@ -191,4 +210,4 @@ const Calculator = () => {
     );
 }
 
-export default Calculator;
+export default App;
